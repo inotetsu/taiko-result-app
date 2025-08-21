cf074079f15faacef3d72ad822af21661dc12b8c
inotetsu <usingprogramming@gmail.com>
2025-08-21 14:46:36 +0900

    Set middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->redirectGuestsTo('/home/login');
+        $middleware->redirectUsersTo('/home');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
--- routes/web.php
+++ routes/web.php
@@ -12,36 +12,36 @@ Route::get('/', function () {
 });
 
 /*曲追加*/
-Route::get('/home',[SongsAddController::class, 'index'])->name('home');
-Route::post('/home/song-add',[SongsAddController::class, 'store'])->name('song.store');
+Route::get('/home',[SongsAddController::class, 'index'])->name('home')->middleware('auth');
+Route::post('/home/song-add',[SongsAddController::class, 'store'])->name('song.store')->middleware('auth');
 
 /*リザルト追加*/
-Route::get('/home/result/{song}',[ResultAddController::class, 'index'])->name('song.result');
-Route::post('/home/result/{song}/store',[ResultAddController::class, 'store'])->name('song.result.store');
+Route::get('/home/result/{song}',[ResultAddController::class, 'index'])->name('song.result')->middleware('auth');
+Route::post('/home/result/{song}/store',[ResultAddController::class, 'store'])->name('song.result.store')->middleware('auth');
 
 /*曲の削除*/
-Route::get('/home/song-delete/{song}',[SongsAddController::class, 'destroy'])->name('song.destroy');
+Route::get('/home/song-delete/{song}',[SongsAddController::class, 'destroy'])->name('song.destroy')->middleware('auth');
 
 /*リザルトの削除*/
-Route::get('/home/result-delete/{song}',[ResultAddController::class, 'destroy'])->name('song.result.destroy');
+Route::get('/home/result-delete/{song}',[ResultAddController::class, 'destroy'])->name('song.result.destroy')->middleware('auth');
 
 /*曲情報の編集*/
-Route::get('/home/song-edit/{song}',[SongsAddController::class, 'edit'])->name('song.edit');
+Route::get('/home/song-edit/{song}',[SongsAddController::class, 'edit'])->name('song.edit')->middleware('auth');
 
 /*曲情報アップデート処理*/
-Route::put('/home/song-update/{song}',[SongsAddController::class, 'update'])->name('song.update');
+Route::put('/home/song-update/{song}',[SongsAddController::class, 'update'])->name('song.update')->middleware('auth');
 
 /*リザルトの編集*/
-Route::get('/home/result-edit/{result}',[ResultAddController::class, 'edit'])->name('song.result.edit');
+Route::get('/home/result-edit/{result}',[ResultAddController::class, 'edit'])->name('song.result.edit')->middleware('auth');
 
 /*リザルトのアップデート処理*/
-Route::put('/home/result-update/{result}',[ResultAddController::class, 'update'])->name('song.result.update');
+Route::put('/home/result-update/{result}',[ResultAddController::class, 'update'])->name('song.result.update')->middleware('auth');
 
 /*リザルト詳細ページ*/
-Route::get('/home/result-detail/{result}',[ResultDetailController::class, 'index'])->name('song.result.detail');
+Route::get('/home/result-detail/{result}',[ResultDetailController::class, 'index'])->name('song.result.detail')->middleware('auth');
 
 /*ログインページ*/
-Route::get('/home/login',[UserController::class, 'index'])->name('login');
+Route::get('/home/login',[UserController::class, 'index'])->name('login')->middleware('guest');
 
 /*新規登録ページ*/
 Route::get('/home/register',[UserController::class, 'create'])->name('register');
